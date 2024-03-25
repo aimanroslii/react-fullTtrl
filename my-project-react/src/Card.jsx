0d80a9e4a309cb02f169f8da8e6e4893276f09af
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-26 00:06:54 +0800

    Creating card components
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

---
+++ my-project-react/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+    return(
+        <div className="card">
+                <img className="card-image" src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" alt="profile picture"></img>
+                <h2 className="card-title">John Wick</h2>
+                <p className="card-text">I make youtube videos and play video game</p>
+        </div>
+    );
+}
+
+export default Card
