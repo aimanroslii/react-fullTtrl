06b3d732542d00140e6e0daf6ea98b4a63d50ff6
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-31 14:13:15 +0800

    Ways on styling CSS in React
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

---
+++ project-lib/src/App.jsx
@@ -0,0 +1,18 @@
+import Button from './Button/Button.jsx'
+import './App.css'
+
+//HOW TO STYLE REACT COMPONENTS WITH CSS
+//1.EXTERNAL
+//2.MODULES
+//3.INLINE
+
+function App() {
+
+  return (
+    <>
+      <Button/>
+    </>
+  )
+}
+
+export default App
