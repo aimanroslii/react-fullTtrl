aab0ad9b53d4bdb650ca1c901b49467af97ec42c
Aiman Rosli <aimanrosli977@yahoo.com>
2024-03-31 14:54:07 +0800

    Conditional rendering in React
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- project-lib/src/App.jsx
+++ project-lib/src/App.jsx
@@ -1,5 +1,6 @@
 import Button from './Button/Button.jsx'
 import Student from './Student.jsx'
+import UserGreeting from './UserGreeting.jsx'
 import './App.css'
 
 //HOW TO STYLE REACT COMPONENTS WITH CSS
@@ -11,14 +12,20 @@ import './App.css'
 //        A parent component can send data to a child component.
 //        <Component key=value /> 
 
+//conditional rendering = allows you to control what gets rendered 
+//                        in your application based on certain conditions
+//                        (show, hide, or change components)
+
 function App() {
 
   return (
     <>
-      <Student name="Spongebob" age = {30} isStudent={true}/>
+      <UserGreeting isLoggedIn = {true} username= "John Wick"/>
+      <UserGreeting isLoggedIn = {true} />
+      {/* <Student name="Spongebob" age = {30} isStudent={true}/>
       <Student name="Patrick" age={42} isStudent={false}/>
       <Student/>
-      {/* <Button/> */}
+      <Button/> */}
     </>
   )
 }
